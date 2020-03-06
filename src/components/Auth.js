a6ec1ecd8c35cdd2608e0d4767f3b26072e9032f
Rico Hancock <richardl@hancock1.com>
2020-03-06 13:47:43 -0700

    Login page updated. General layout and some styling

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -3,36 +3,48 @@ import axios from 'axios';
 import {connect} from 'react-redux';
 import {getUser} from '../redux/userReducer';
 const Auth = props => {
-    const [emailInput, setEmailInput] = useState('');
-    const [passwordInput, setPasswordInput] = useState('');
-    const login = () => {
-        axios.post('/auth/login', {email: emailInput, password: passwordInput})
-        .then(res => {props.getUser(res.data)
-        props.history.push('/homemain')})
-        .catch(err => {window.alert('Email/password incorrect')});
-    }
-    const register = () => {
-        axios.post('/auth/register', {email: emailInput, password: passwordInput})
-        .then(res => {props.getUser(res.data)
-        window.alert('Register accepted, please login')})
-        .catch(err => {window.alert('Email/password exists')});
-    }
+  const [loginClass, setLoginClass] = useState('login-container')
+  const [registerClass, setRegisterClass] = useState('register-container')
     return(
         <main>
-          Login
-        {/* <div className='login-box'>
-            <input type='text' placeholder='Email' className='login-input'
-                value={emailInput} onChange={(e) => setEmailInput(e.target.value)}
-            />
-            <input type='password' placeholder='Password' className='login-input' 
-                value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)}
-            />
-            <div className='flex-login-buttons'>
-            <button className='login-button' onClick={() => login()}>Login</button>
-            <p className='or-word'>Or</p>
-            <button className='register-button' onClick={() => register()}>Register</button>
+          <div className='auth-container'>
+            <div className={loginClass}>
+              <h3>Login</h3>
+              <div className='login-row'>
+                <p>Email: </p>
+                <input type='text'/>
+              </div>
+              <div className='login-row'>
+                <p>Password: </p>
+                <input type='password'/>
+              </div>
+              <div className='login-row'>
+                <button onClick={() => {
+                  setLoginClass('login-container left')
+                  setRegisterClass('register-container left')
+                }}>Register</button>
+                <button>Login</button>
+              </div>
             </div>
-        </div> */}
+            <div className={registerClass}>
+              <h3>Register</h3>
+              <div className='login-row'>
+                <p>Email: </p>
+                <input type='text'/>
+              </div>
+              <div className='login-row'>
+                <p>Password: </p>
+                <input type='password'/>
+              </div>
+              <div className='login-row'>
+                <button onClick={() => {
+                  setLoginClass('login-container')
+                  setRegisterClass('register-container')
+                }}>back to Login</button>
+                <button>Register</button>
+              </div>
+            </div>
+          </div>
         </main>
     );
 }
