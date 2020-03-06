6d16bf3bff07e46bc6c35bbacf2ebe05911de9d4
Zachary Johnson <zach_j@att.net>
2020-03-06 11:12:29 -0700

    auth control done

---
+++ DB/register_user.sql
@@ -0,0 +1,24 @@
+insert into patients (
+    first_name,
+    last_name,
+    email,
+    password,
+    phone_number,
+    address,
+    birth_date,
+    religious_preference,
+    blood_type,
+    is_patient
+) values (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8,
+    $9,
+    $10
+)
+returning *;
