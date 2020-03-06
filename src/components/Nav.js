64ccb1073929539ed886ca2a6140cfdb75249cbc
Rico Hancock <richardl@hancock1.com>
2020-03-06 16:18:51 -0700

    navbar updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,37 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+
 class Nav extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            toggleDropdown: false
+            toggleDropdown: false,
+            // body: document.getElementsByTagName('body'),
         }
     }
     render(){
+        
+        // this.state.body.onclick = this.setState({toggleDropdown: false})
         return(
             <nav>
                 <div className='icons'>
-                    <i className="fas fa-clinic-medical"></i>
-                    <i className="fas fa-ambulance"></i>
+                    <Link to='/dashboard' ><i className="fas fa-clinic-medical"></i></Link>
+                    <Link to='/first-responder' ><i className="fas fa-ambulance"></i></Link>
                 </div>
                 <div>
                     <p>Hello, first_name.</p>
                     <i className="fas fa-bars" onClick={() => this.setState({toggleDropdown: !this.state.toggleDropdown})}></i>
                     {this.state.toggleDropdown ? <div className='dropdown'>
-                        <Link to='/dashboard'>Dashboard</Link>
-                        <Link to='/meds'>Medications</Link>
-                        <Link to='/allergies'>Allergies</Link>
-                        <Link to='/surgeries'>Surgeries</Link>
-                        <Link to='/med-history'>Medical History</Link>
-                        <Link to='/calendar'>Calendar</Link>
-                        <Link to='/pcare'>Primary Care Physician</Link>
-                        <Link to='/profile'>Profile</Link>
+                        <Link to='/dashboard' onClick={() => this.setState({toggleDropdown: false})}>Dashboard</Link>
+                        <Link to='/meds' onClick={() => this.setState({toggleDropdown: false})}>Medications</Link>
+                        <Link to='/allergies' onClick={() => this.setState({toggleDropdown: false})}>Allergies</Link>
+                        <Link to='/surgeries' onClick={() => this.setState({toggleDropdown: false})}>Surgeries</Link>
+                        <Link to='/med-history' onClick={() => this.setState({toggleDropdown: false})}>Medical History</Link>
+                        <Link to='/calendar' onClick={() => this.setState({toggleDropdown: false})}>Calendar</Link>
+                        <Link to='/primary-care' onClick={() => this.setState({toggleDropdown: false})}>Primary Care Physician</Link>
+                        <Link to='/profile' onClick={() => this.setState({toggleDropdown: false})}>Profile</Link>
                         {/* <Link to='/logout'>Logout</Link> */}
                     </div> : null}
                 </div>
