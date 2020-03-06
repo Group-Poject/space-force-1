44e3ab7498fff5d7a44e484d11a9e925147f414d
Rico Hancock <richardl@hancock1.com>
2020-03-06 11:32:02 -0700

    Nav, Auth, and Footer started. Basic styling

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+    render(){
+        return(
+            <footer>
+                <div>
+                  <a>About</a>
+                  <a>Contact</a>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Nav;
