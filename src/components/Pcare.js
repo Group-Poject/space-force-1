6a99f09c4be5c9ff4e63283a6335f9e1150cca8e
Zachary Johnson <zach_j@att.net>
2020-03-06 14:20:18 -0700

    general push

--- src/components/Pcare.js
+++ src/components/Pcare.js
@@ -1,65 +1,86 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import {useInputValue} from '../hooks/useInputValue'
+import React, { useState } from "react";
+import axios from "axios";
+import { useInputValue } from "../hooks/useInputValue";
 
 const PCare = props => {
-    const {showAdd, setShowAdd} = useState(false)
-    const {primary, setPrimary} = useState({})
-    const firstName = useInputValue('')
-    const lastName = useInputValue('')
-    const number = useInputValue(0)
-    const email = useInputValue('')
-    const address = useInputValue('')
+  const { showAdd, setShowAdd } = useState(false);
+  const { primary, setPrimary } = useState({});
+  const [
+    { firstName, lastName, number, email, address },
+    setValues
+  ] = useInputValue({
+    firstName: "",
+    lastName: "",
+    number: 0,
+    email: "",
+    address: ""
+  });
 
+  addProvider = (
+    patientId,
+    firstName,
+    lastName,
+    phoneNumber,
+    email,
+    address
+  ) => {
+    axios
+      .post("/api/provider", {
+        patientId,
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        address
+      })
+      .then(res => {
+        setPrimary(res.data);
+      })
+      .catch(err => console.log(err));
+  };
 
-    addProvider = (patientId, firstName, lastName, phoneNumber, email, address) => {
-        axios.post('/api/provider', {patientId, firstName, lastName, phoneNumber, email, address}).then(res => {
-            setPrimary(res.data)
-        }).catch(err => console.log(err))
-    }
-    
-        return(
-            <main>
-                <button onClick={setShowAdd(!showAdd)} >Add Provider</button>
-                <div>name | address| etc</div>
-                <div>map</div>
-                {showAdd && (
-                    <div>
-                        <input
-                        placeholder='First Name'
-                        type='text'
-                        value={firstName}
-                        onChange={...firstName}
-                        />
-                        <input
-                        placeholder='Last Name'
-                        type='text'
-                        value={lastName}
-                        onChange={...lastName}
-                        />
-                        <input
-                        placeholder='Phone Number'
-                        type='number'
-                        value={number}
-                        onChange={...number}
-                        />
-                        <input
-                        placeholder='Email'
-                        type='email'
-                        value={email}
-                        onChange={...email}
-                        />
-                        <input
-                        placeholder='address'
-                        type='text'
-                        value={address}
-                        onChange={...address}
-                        />
-                        <button>Submit</button>
-                    </div>
-                )}
-            </main>
-        )
-    }
+  return (
+    <main>
+      <button onClick={setShowAdd(!showAdd)}>Add Provider</button>
+      <div>name | address| etc</div>
+      <div>map</div>
+      {showAdd && (
+        <div>
+          <input
+            placeholder="First Name"
+            type="text"
+            value={firstName}
+            onChange={setValues}
+          />
+          <input
+            placeholder="Last Name"
+            type="text"
+            value={lastName}
+            onChange={setValues}
+          />
+          <input
+            placeholder="Phone Number"
+            type="number"
+            value={number}
+            onChange={setValues}
+          />
+          <input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={setValues}
+          />
+          <input
+            placeholder="address"
+            type="text"
+            value={address}
+            onChange={setValues}
+          />
+          <button onClick={} >Submit</button>
+        </div>
+      )}
+    </main>
+  );
+};
 
-    export default PCare; 
+export default PCare;
