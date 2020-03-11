17e6136802fe5389b7b093a7fff18a2b50ec7c48
Zachary Johnson <zach_j@att.net>
2020-03-11 11:18:37 -0600

    basic DB calls done

---
+++ server/controllers/contactsCtrl.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  getContacts: (req, res) => {
+    const { id } = req.params;
+    const db = req.app.get("db");
+    db.contacts
+      .get_contacts(id)
+      .then(data => res.status(200).send(data))
+      .catch(err => res.status(500).send(err));
+  },
+
+  addContact: (req, res) => {
+    const { id } = req.params;
+    const { email, password, phone_number, has_access } = req.body;
+    const db = req.app.get("db");
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    db.contacts
+      .add_contact(email, hash, id, phone_number, has_access)
+      .then(data => res.status(200).send(data))
+      .catch(err => res.status(500).send(err));
+  },
+
+  editContact: (req, res) => {
+    const { id } = req.params;
+    const { email, password, phone_number, has_access } = req.body;
+    const db = req.app.get("db");
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    db.contacts
+      .edit_contacts(email, hash, id, phone_number, has_access)
+      .then(data => res.status(200).send(data))
+      .catch(err => res.status(500).send(err));
+  },
+
+  deleteContact: (req, res) => {
+    const { id } = req.params;
+    const db = req.app.get("db");
+    db.contacts
+      .delete_contact(id)
+      .then(data => res.status(200).send(data))
+      .catch(err => res.status(500).send(err));
+  }
+};
