f46817cfe57e85069ac92accb9e510ba29316b60
Kori Gandolph <korigandolph@Koris-MacBook-Air.local>
2020-03-12 16:48:41 -0600

    pcare

--- src/components/Pcare.js
+++ src/components/Pcare.js
@@ -1,92 +1,113 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useInputValue } from "../hooks/useInputValue";
+import React, {useState, useEffect} from 'react';
+import { useInputValue } from '../hooks/useInputValue';
+import axios from 'axios';
+import {connect} from 'react-redux';
+import {getUser} from '../redux/userReducer';
 
-const PCare = props => {
-  const { showAdd, setShowAdd } = useState(false);
-  const { primary, setPrimary } = useState({});
-  const [
-    { firstName, lastName, number, email, address },
-    setValues
-  ] = useInputValue({
-    firstName: "",
-    lastName: "",
-    number: 0,
-    email: "",
-    address: ""
-  });
+const Pcare = props => {
+    const [{first_name, last_name, phone_number, email, address}, setValues, resetValues]=useInputValue({
+        first_name: '',
+        last_name: '',
+        phone_number: '',
+        email: '',
+        address: ''
+    },[])
+    const [pcare_list, setList]=useState([])
+    const getPcare = () => axios.get(`/api/pcare${props.user.patient_id}`).then(results=>setList(results.data)).catch(err=>console.log(err))
+    useEffect((e)=>{
+        // console.log('hit')
+        getPcare();
+    }, [])
+    const deletePcare =(id)=>{
+        console.log(id)
+        axios.delete (`/api/pcare/${id}`)
+        .then(results=> {
+          getPcare()
+        }).catch(err=>console.log(err))
+    }
+    const [toggle, setToggle]=useState(false);
+        return(
+            <main id='meds-main'>
+                <form 
+                    onSubmit={e=>{
+                        e.preventDefault()
+                        axios.post(`/api/pcare${props.user.patient_id}`, {first_name, last_name, phone_number, email, address}).then(results=>{
+                            setList(results.data)
+                            resetValues();
+                            getPcare();             
+                    })
+                            .catch(err=>console.log(err))}
+                    }>
 
-  const addProvider = (
-    patientId,
-    firstName,
-    lastName,
-    phoneNumber,
-    email,
-    address
-  ) => {
-    axios
-      .post("/api/provider", {
-        patientId,
-        firstName,
-        lastName,
-        phoneNumber,
-        email,
-        address
-      })
-      .then(res => {
-        setPrimary(res.data);
-      })
-      .catch(err => console.log(err));
-  };
+                    <div className='add-medication-container'>  
+                        <h3>Primary Care Physician</h3> 
+                        <div>
+                            <input 
+                            name='first_name'
+                            placeholder='First Name'
+                            value={first_name}
+                            onChange={setValues}
+                            />
+                            <input 
+                            name='last_name'
+                            placeholder='Last Name'
+                            value={last_name}
+                            onChange={setValues}
+                            />
+                            <input 
+                            name='phone_number'
+                            placeholder='Phone Number'
+                            value={phone_number}
+                            onChange={setValues}
+                            />
+                            <input 
+                            name='email'
+                            placeholder='Email'
+                            value={email}
+                            onChange={setValues}
+                            />
+                            <input 
+                            name='address'
+                            placeholder='Address'
+                            value={address}
+                            onChange={setValues}
+                            />
+                            <button type='submit'>Add New</button>
+                        </div>
+                    </div>
+                    </form>
+                    <div className='medication-container'>
+                        <div className='meds-header'>
+                            <p>Name</p>
+                            <p>Phone Number</p>
+                            <p>Email</p>
+                            <p>Address</p>
+                        </div>
+                    {pcare_list.map((pcare, index)=>(
+                        <div key={index} className='med-history'>
+                            <p className='med-action'>{pcare.first_name} {pcare.last_name}<p>{toggle ? <actionbutton onClick={()=> {
+                            setToggle(!toggle)
+                            // editAllergy(allergy)
+                            }}>Save</actionbutton>:<actionbutton onClick={()=>setToggle(!toggle)}>Edit</actionbutton>} 
+                            <actionbutton
+                            onClick={() => {
+                                console.log(pcare)
+                                deletePcare(pcare.provider_id)
+                            }}
+                            >Delete </actionbutton></p></p>
+                            <p>{pcare.phone_number}</p>
+                            <p>{pcare.email}</p>
+                            <p>{pcare.address}</p>
+                                    
+                        </div>
+                    ))}
+                </div>
+            </main>
+        )
+    }
 
-  return (
-    <main id='provider-main'>
-      <button onClick={() => setShowAdd(!showAdd)}>Add Provider</button>
-      <div className='prov-info-container'>
-        <div>
-            <p>Name</p>    
-            <p>Address</p>    
-            <p>Etc</p>    
-        </div>
-        <div>map</div>
-      </div>
-      {showAdd && (
-        <div>
-          <input
-            placeholder="First Name"
-            type="text"
-            value={firstName}
-            onChange={setValues}
-          />
-          <input
-            placeholder="Last Name"
-            type="text"
-            value={lastName}
-            onChange={setValues}
-          />
-          <input
-            placeholder="Phone Number"
-            type="number"
-            value={number}
-            onChange={setValues}
-          />
-          <input
-            placeholder="Email"
-            type="email"
-            value={email}
-            onChange={setValues}
-          />
-          <input
-            placeholder="address"
-            type="text"
-            value={address}
-            onChange={setValues}
-          />
-          <button >Submit</button>
-        </div>
-      )}
-    </main>
-  );
-};
-
-export default PCare;
+    function mapStateToProps (state) {
+        return {user: state.userReducer.user}
+    }
+    
+    export default connect(mapStateToProps, {getUser})(Pcare);
