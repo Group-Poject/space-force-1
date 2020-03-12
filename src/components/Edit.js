3494bfe90175018d9880918b9d57f0f776ecf1f3
Zachary Johnson <zach_j@att.net>
2020-03-12 15:30:31 -0600

    creating edit component

---
+++ src/components/Edit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Edit = props => {
+
+    const displayInputs = props => {
+        // let display = props.
+    }
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Edit;
